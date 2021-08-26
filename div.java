bb068b3832e8ce17bf4e39c904d47b0f235d999b
Oishee Majumder <oisheemajumder36@gmail.com>
2021-08-26 23:09:34 +0530

    div.java second commit

---
+++ div.java
@@ -0,0 +1,15 @@
+public class div {
+
+    public static void main(String args[])
+
+    {
+
+        int a=60;
+        int b=12;
+        int c= a/b;
+
+        System.out.println(c);
+
+    }
+    
+}
