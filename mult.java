63368813d5512cef26784b357c7c608cb956ea93
Oishee Majumder <oisheemajumder36@gmail.com>
2021-08-26 23:08:23 +0530

    mult java add first commit

---
+++ mult.java
@@ -0,0 +1,16 @@
+public class mult {
+
+    
+
+        public static void main(String args[]) {
+           
+       int s=1;
+       int p=2;
+       int m=s*p;
+       System.out.println("the answer is "+m);
+           
+       }
+       
+   
+    
+}
